12911bcad6bf5a8cb0ba5b651290c30b32e83d85
Rylan Bauermeister <rylan@bauermeister.com>
2019-03-12 00:17:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,6 +118,14 @@ def game_hash
   }
 end
 
+def get_player(name)
+  if game_hash[:home][:players].include?(name)
+    return game_hash[:home][:players][name]
+  else 
+    return game_hash[:away][:players][name]
+  end
+end
+
 def num_points_scored(name)
   if game_hash[:home][:players].include?(name)
     return game_hash[:home][:players][name][:points]
